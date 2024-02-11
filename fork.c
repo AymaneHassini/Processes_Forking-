a3eb00cac75662d900d1f9573b58b34f9d892432
AymaneHassini <aymane.hassini@gmail.com>
2024-02-11 18:55:02 +0100

    creating a process using execlp, running a terminal command in the child process

--- fork.c
+++ fork.c
@@ -5,27 +5,36 @@
 int main() {
   pid_t pid;
 
-  // Fork a child process
+  // fork a child process
   pid = fork();
 
   if (pid == 0) {
-    // This is the child process
+    // this is the child process
     printf("This is the child process (PID: %d)\n", getpid());
-    _exit(0); // Terminate child process
+    // use execlp to replace child process image with "ls -l" command
+        int exec_ret = execlp("ls", "ls", "-l", NULL);
+     // handle potential errors in execlp
+        if (exec_ret == -1) {
+            perror("execlp");
+            _exit(1); //exit the child process with error
+        }
   } else if (pid > 0) {
-    // This is the parent process
+    // parent process
     int status;
-
-    // Wait for the child process to finish
+    // wait for child process to finish
     wait(&status);
-
-    printf("This is the parent process (PID: %d)\n"
-       "Child process (PID: %d) terminated.\n", getpid(), pid);  }
-        else {
-    // Fork failed
+    // used of WIFEXITED  to check if the child exited normally
+    // WEXITSTATUS is to get the exit code (0 for success).
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            printf("Child process (PID: %d) terminated successfully.\n", pid);
+        } else {
+            printf("Child process (PID: %d) terminated with error.\n", pid);
+        }
+  }else{
+    //pid<0
+    //fork failed
     perror("fork");
     return 1;
   }
-
   return 0;
 }

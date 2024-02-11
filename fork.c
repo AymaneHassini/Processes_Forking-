e5c85e6390e5f2344efb57a5cdee7a10e8795460
AymaneHassini <aymane.hassini@gmail.com>
2024-02-11 20:50:21 +0100

    combining fork and execlp in the same program.

--- fork.c
+++ fork.c
@@ -2,27 +2,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main() {
+int main()
+{
   pid_t pid;
 
   // fork a child process
   pid = fork();
-  //initialize a variable to 10
-  int var=10;
-  if (pid == 0) {
-    // this is the child process
-    printf("Child PID: %d, Variable value: %d\n", getpid(),var);
-    int var=20;
-    printf("Child After change, Variable value: %d\n", var);
-  } else if (pid > 0) {
+  // initialize a variable to 10
+  if (pid == 0)
+  {
+    execlp("ls", "ls", "-l", NULL); // Replace child process with "ls" command
+    perror("execlp failed");  // This line won't execute if execlp succeeds
+  }
+  else if (pid > 0)
+  {
     // parent process
-   printf("Parent PID: %d, Variable value: %d\n", getpid(), var);
-    var = 30; // Change variable in parent
-    printf("Parent After change, Variable value: %d\n", var);
-  }else{
-    //pid<0
-    //fork failed
-    perror("fork");
+    printf("Parent process still running (PID: %d)\n", getpid());
+    wait(NULL); // Wait for child process to finish
+  }
+  else
+  {
+    // pid<0
+    // fork failed
+    perror("fork failed");
     return 1;
   }
   return 0;
